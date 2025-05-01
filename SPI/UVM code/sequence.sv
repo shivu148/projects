60153a804e3ef7fb23e49c464507134ffe08f5f0
SHIVARAJ D A <shivarajda2020@gmail.com>
2025-05-01 12:21:14 +0530

    Create sequence.sv

---
+++ SPI/UVM code/sequence.sv
@@ -0,0 +1,23 @@
+
+
+class spi_sequence extends uvm_sequence#(spi_seq_item);
+  
+  `uvm_object_utils(spi_sequence)
+  
+ 
+  function new(string name = "spi_sequence");
+    super.new(name);
+  endfunction
+  
+ 
+  task body();
+    spi_seq_item seq;
+    repeat(10)begin
+      seq=new();
+      start_item(seq);
+      assert(seq.randomize());
+      finish_item(seq);
+    end
+  endtask
+    
+endclass
