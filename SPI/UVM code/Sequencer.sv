8f3845c7a0b907d66b895597303cd343260c6658
SHIVARAJ D A <shivarajda2020@gmail.com>
2025-05-01 12:21:46 +0530

    Create Sequencer.sv

---
+++ SPI/UVM code/Sequencer.sv
@@ -0,0 +1,16 @@
+
+
+class spi_sequencer extends uvm_sequencer#(spi_seq_item);
+  
+  `uvm_component_utils(spi_sequencer)
+  
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+  
+endclass
